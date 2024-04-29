3a0576e4ed83167b1b612222b7db440ddf28f108
Aaron Stone <apstone@protonmail.com>
2024-04-29 12:12:11 -0400

    fix bufferline

--- init.lua
+++ init.lua
@@ -880,8 +880,8 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.bufferline',
-  require 'custom.plugins.mini-surround',
   require 'custom.plugins.lazygit',
+  require 'custom.plugins.nui',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
